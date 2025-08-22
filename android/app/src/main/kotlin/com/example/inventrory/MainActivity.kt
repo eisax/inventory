52f1f1f7cac1e83d3f8140e265282fda6d5c063f
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-08-22 10:54:38 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/inventrory/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.inventrory
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/hopescreen.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.menu),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 30,
+                        height: 30,
+                        child: Icon(Icons.search),
+                      ),
+                      SizedBox(
+                        width: 30,
+                        height: 30,
+                        child: Icon(Icons.notifications),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 8),
+              Row(
+                children:
+                    ["New Requests", "Orders Pending", "Revenue"]
+                        .map(
+                          (e) => Container(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 16.0,
+                              vertical: 8.0,
+                            ),
+                            margin: EdgeInsets.only(right: 8.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              border:
+                                  e == "New Requests"
+                                      ? null
+                                      : Border.all(
+                                        width: 0.25,
+                                        color: Color(0xFF121212),
+                                      ),
+                              color:
+                                  e == "New Requests"
+                                      ? Colors.blueAccent
+                                      : Colors.transparent,
+                            ),
+                            child: Text(
+                              e,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 12,
+                                color:
+                                    e == "New Requests"
+                                        ? Colors.white
+                                        : Color(0xFF121212),
+                              ),
+                            ),
+                          ),
+                        )
+                        .toList(),
+              ),
+              SizedBox(height: 8),
+              Expanded(
+                child: Container(
+                  color: Colors.grey.shade200,
+                  child: Center(child: Text("Content Area")),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
